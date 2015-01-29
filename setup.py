6d52272006003acce87f0dc607af2fa4998a78e4
Andrey Antukh <niwi@niwi.be>
2015-01-29 09:01:52 +0100

    Increment version to 1.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url = "https://github.com/niwibe/py-couchdb",
     author = "Andrey Antukh",
     author_email = "niwi@niwi.be",
-    version="1.9",
+    version="1.10",
     packages = [
         "pycouchdb",
     ],
