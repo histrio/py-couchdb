8a712763f8a38b753ff6a43824d0a5e1537d9e65
Pawel Krawczyk <p.krawczyk@kainos.com>
2015-04-26 15:58:52 +0100

    reformat

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from setuptools import setup
 description = "Modern pure python CouchDB Client."
 
 try:
-   from distutils.command.build_py import build_py_2to3 as build_py
+    from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
     from distutils.command.build_py import build_py
 
 setup(
-    name = "pycouchdb",
-    url = "https://github.com/niwibe/py-couchdb",
-    author = "Andrey Antukh",
-    author_email = "niwi@niwi.be",
+    name="pycouchdb",
+    url="https://github.com/niwibe/py-couchdb",
+    author="Andrey Antukh",
+    author_email="niwi@niwi.be",
     version="1.9",
-    packages = [
+    packages=[
         "pycouchdb",
     ],
-    description = description.strip(),
-    zip_safe = False,
-    include_package_data = True,
-    classifiers = [
+    description=description.strip(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "License :: OSI Approved :: BSD License",
@@ -33,11 +33,11 @@ setup(
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
     ],
-    entry_points = {
+    entry_points={
         "console_scripts": [
             "couchpy = pycouchdb.view:main"
         ],
     },
-    cmdclass = {"build_py": build_py},
-    install_requires = ["requests"]
+    cmdclass={"build_py": build_py},
+    install_requires=["requests"]
 )
