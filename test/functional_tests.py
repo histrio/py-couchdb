b5a29645ec88cd0b2e3f010240c78a2f6f7b0c33
Rinat Sabitov <rinat.sabitov@gmail.com>
2015-11-09 16:06:36 +0300

    Added some simple tests

--- test/functional_tests.py
+++ test/functional_tests.py
@@ -9,26 +9,54 @@ Description:
 import responses
 import pycouchdb
 
-
-def get_resp_mock():
-    resp = mock.Mock()
-    resp.headers = {}
-    resp.body = bytes(0)
-    resp.raw.data = bytes(0)
-    resp.status_code = 200
-    return resp
+from pycouchdb.exceptions import Conflict
+from nose.tools import raises
 
 
-# @mock.patch('pycouchdb.resource.requests.session')
-@responses.activate
-def test_test():
-    responses.add(
-        responses.GET,
-        "http://example.com/",
-        body='[{"title": "Test Deal"}]',
-        content_type="application/json"
-    )
-    server = pycouchdb.Server("http://example.com/")
-    result = server.info()
+class TestServer:
 
-    assert result==[{'title': 'Test Deal'}]
+    def setup(self):
+        self.server = pycouchdb.Server("http://example.com/")
+
+    def test_server_info(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(
+                responses.GET,
+                "http://example.com/",
+                body='{"version": "1.2.1"}',
+                content_type="application/json"
+            )
+            result = self.server.info()
+            assert result['version'] == '1.2.1'
+
+    def test_server_iter(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(
+                responses.GET,
+                "http://example.com/_all_dbs",
+                body='["_replicator","_users","bsdp_v2"]',
+                content_type="application/json"
+            )
+            assert len(self.server) > 0
+
+    def test_create_db(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.PUT, "http://example.com/testing1")
+            rsps.add(responses.HEAD, "http://example.com/testing1")
+            self.server.create("testing1")
+
+    def test_delete_db(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.DELETE, "http://example.com/testing1")
+            self.server.delete("testing1")
+
+    @raises(Conflict)
+    def test_create_db_conflict(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.PUT, "http://example.com/testing1")
+            rsps.add(responses.HEAD, "http://example.com/testing1")
+            self.server.create("testing1")
+            rsps.add(responses.PUT, "http://example.com/testing1",
+                    content_type="application/json",
+                    body='{"error":"file_exists"}', status=409)
+            self.server.create("testing1")
